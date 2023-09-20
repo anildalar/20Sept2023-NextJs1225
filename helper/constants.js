385e8878fd1e62cab43cd449fe32f96d5291b493
anildalar <dollorinfotech@gmail.com>
2023-09-20 10:02:12 +0530

    new

---
+++ helper/constants.js
@@ -0,0 +1,11 @@
+//1. Import Area
+
+
+//2 Defination Area
+const TITLE = 'Justdial - Local Search, Social, News, Videos, Shopping'
+
+//3 Export Area
+//3.1 Default Export
+export default TITLE;
+
+//3.2 Named Export
---
+++ helper/contacts2.js
@@ -0,0 +1,10 @@
+//1. Import Area
+
+
+//2. Defination Area
+const DESCRIPTION = `Justdial, India's local search engine, provides Best Deals, Shop Online, Ticket Booking for Flights, Hotels, Movies, Buses and Cabs. You can also Order Food, Book Restaurant Table, View Menu, Book Doctors’ Appointments. Justdial curates Social content, News, Videos & more from Top Publishers on all Trending Topics.`;
+
+
+//3. Export Area
+//3.2 Named Export
+module.exports = { DESCRIPTION }
